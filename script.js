1aa55fb8dc18604017ac05b99e375e1417dad2ec
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-09-17 22:01:18 -0700

    Adding local storage memory functionality to save event inputs from user.

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 var currentTime = moment();
-$("#currentDay").text("Today is " + currentTime.format("MMM Do, YYYY"));
+$("#currentDay").text("Today is " + currentTime.format("dddd"));
+$("#currentDate").text(currentTime.format("MMMM Do, YYYY"));
 
+//Functional digital clock in the header
 setInterval(() => {
-    document.getElementById("headerClock").textContent = `${moment().format("LTS")}`;
+  document.getElementById("headerClock").textContent = `${moment().format(
+    "LTS"
+  )}`;
 }, 1000);
+
+var dailyHours = 8;
+
+var textEdit = document.getElementById("addEventText");
+var saveButton = document.getElementById("saveButt1");
+
+function saveLastEvent() {
+  var saveNewEvent = textEdit.value;
+  localStorage.setItem("saveNewEvent", JSON.stringify(saveNewEvent));
+}
+
+function currentEvent() {
+  var currentEventText = JSON.parse(localStorage.getItem("saveNewEvent"));
+  if (currentEventText !== null) {
+    textEdit.innerHTML = currentEventText;
+  } else {
+    return;
+  }
+}
+
+saveButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  saveLastEvent();
+  currentEvent();
+});
+
+function init() {
+  currentEvent();
+}
+
+init();
